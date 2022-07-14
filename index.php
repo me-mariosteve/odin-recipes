7bb6d6ddf5b12203414d53ad256c35a03f84691c
me-mariosteve <mariosteve069@gmail.com>
2022-07-14 19:12:58 +0200

    use php,new sidebar and stuff

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Odin Recipes</title>
+		<link rel="stylesheet" href="styles.css">
+	</head>
+	<body>
+		<?php include 'sidebar.php'; ?>
+	</body>
